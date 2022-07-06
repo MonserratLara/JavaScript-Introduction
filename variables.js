8aa690bdded8cd42f36ef513063ebaccac4a5691
MonserratLara <mlara@guilford.edu>
2022-07-05 23:12:08 -0400

    My Second commit

---
+++ variables.js
@@ -0,0 +1,107 @@
+var mynum1 = 50;
+var mynum2 = 13;
+var mynum3 = mynum1 - mynum2;
+
+console.log(`The diffrence between ${mynum1} and ${mynum2} is ${mynum3}`)
+
+
+
+
+
+
+
+var person1= "Monse";
+var person2= "Mia";
+
+if(person1.length > person2.length){
+    console.log(`${person1}'s name is longer ${person2}'s name ${person1.length - person2.length} characters`);
+} 
+
+
+
+
+
+
+<script>
+// define the variable for message
+var medium = prompt("Enter the message");
+//if string equals the string in uppercase
+if(medium == medium.toUpperCase())
+{
+    //someone is shouting
+    console.log("Someone is shouting")
+}
+//else if string equals the string in lowercase
+else if(medium == medium.toLowerCase())
+{
+    //someone is whispering
+    console.log("Someone is whispering")
+}
+//else niether
+else
+{
+    console.log("Someone is neither whispering nor shouting")
+}
+</script>
+
+
+
+
+
+
+function add(x, y) {
+    return x + y;
+    }
+    function subtract(x, y) {
+    return x - y;
+    }
+    function multiply(x, y) {
+    return x * y;
+    } 
+    function divide(x, y) {
+    return x / y;
+    }
+    var z=add(10,2)
+    console.log(z);
+
+
+
+
+
+
+    function add(x, y){
+
+        return x + y;
+        
+        }
+        
+        function subtract(x, y){
+        
+        return x - y;
+        
+        }
+        
+        function multiply(x, y){
+        
+        return x * y;
+        
+        }
+        
+        function divide(x, y){
+        
+        return x / y;
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
